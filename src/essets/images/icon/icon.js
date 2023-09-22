39f8dffefc0f6dd1fd262ed03cc6b17f51c16f30
yelim <dp0808@naver.com>
2023-09-22 15:59:56 +0900

    이미지 추가

---
+++ src/essets/images/icon/icon.js
@@ -0,0 +1,15 @@
+import js from "./js.png";
+import react from "./react.png";
+import html5 from "./html5.png";
+import css3 from "./css3.png";
+import sass from "./sass.png";
+
+const icons = {
+  js,
+  react,
+  html5,
+  css3,
+  sass,
+};
+
+export default icons;
